867c6d6ebab4e0299746146f3392ed6f43105ab6
Larry Singleton <Larry.Singleton@MutualOfOmaha.com>
2019-07-19 18:21:01 -0500

    update jca script

--- mubench/findJCA.sh
+++ mubench/findJCA.sh
@@ -2,11 +2,14 @@
 
 > jcaCounts
 
+echo "| Name | JCA | SKS |" | tee -a jcaCounts
+echo "| ---- | :-: | :-: |" | tee -a jcaCounts
+
 list=$(find . -mindepth 1 -maxdepth 1 -type d | sort)
 for project in $list
 do
 	JAVA=$(find $project -type f -name \*.java)
 	jca=$(grep javax.crypto $JAVA | wc -l)
 	sks=$(grep "new SecretKeySpec" $JAVA | wc -l)
-	echo "$(basename $project) $jca $sks"  | tee -a jcaCounts
+	echo "| $(basename $project) | $jca | $sks |"  | tee -a jcaCounts
 done
