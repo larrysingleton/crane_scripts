8a6e621204f18ee0e49a77bd3cbd0a342365085a
Larry Singleton <Larry.Singleton@MutualOfOmaha.com>
2019-08-25 12:54:11 -0500

    updates

---
+++ mubench/findJCA2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+> jcaCounts
+
+echo "| Name | JCAc | JCAi | SKSi |" | tee -a jcaCounts
+echo "| ---- | :-: | :-:  | :-: |" | tee -a jcaCounts
+
+list=$(find . -mindepth 1 -maxdepth 1 -type d | sort)
+for project in $list
+do
+	JAVA=$(find $project -type f -name \*.java)
+
+	# number of classes using JCA
+	jca1=$(grep -c javax.crypto $JAVA | grep -v :0$ | wc -l)
+
+	# number of instances of JCA
+	jca2=$(grep javax.crypto $JAVA | wc -l)
+
+	# number of instances overall of SKS
+	sks=$(grep "new SecretKeySpec" $JAVA | wc -l)
+	echo "| $(basename $project) | $jca1 | $jca2 | $sks |"  | tee -a jcaCounts
+done
