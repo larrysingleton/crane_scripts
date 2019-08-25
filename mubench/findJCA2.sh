9a15452a8450630c97bd63dd4db8c700c7c3a6d6
Larry Singleton <Larry.Singleton@MutualOfOmaha.com>
2019-08-25 13:21:19 -0500

    updates

--- mubench/findJCA2.sh
+++ mubench/findJCA2.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+GRPS=/cygdrive/c/data/git-uno/crane_scripts/group/groups
+
 OUT=jcaCounts.md
 > $OUT
 
-echo "| Name | JCAc | JCAi | SKSi |" | tee -a $OUT
-echo "| ---- | :-: | :-:  | :-: |" | tee -a $OUT
+echo "| Name | Group | JCAc | JCAi | SKSi |" | tee -a $OUT
+echo "| ---- | :-:   | :-:  | :-:  | :-:  |" | tee -a $OUT
 
 list=$(find . -mindepth 1 -maxdepth 1 -type d | sort)
 for project in $list
 do
+
+	# get the group name
+	BPRJ=$(basename $project)
+	PRJ=$(echo $BPRJ | cut -d'_' -f2-)
+	GRP=$(grep "^${PRJ}$(printf '\t')" $GRPS | awk '{print $2}')
+
+	# list of java files
 	JAVA=$(find $project -type f -name \*.java)
 
 	# number of classes using JCA
@@ -19,5 +28,5 @@ do
 
 	# number of instances overall of SKS
 	sks=$(grep "new SecretKeySpec" $JAVA | wc -l)
-	echo "| $(basename $project) | $jca1 | $jca2 | $sks |"  | tee -a $OUT
+	echo "| $BPRJ | $GRP | $jca1 | $jca2 | $sks |"  | tee -a $OUT
 done
